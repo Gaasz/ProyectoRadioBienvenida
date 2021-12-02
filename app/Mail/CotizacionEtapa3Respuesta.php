0bf9b424c2410e903558eecd6bda6a8c145c0b78
Gaasz <gaston584@gmail.com>
2021-12-02 12:34:04 -0300

    arreglo de los changes

--- app/Mail/CotizacionEtapa3Respuesta.php
+++ app/Mail/CotizacionEtapa3Respuesta.php
@@ -11,7 +11,6 @@ class CotizacionEtapa3Respuesta extends Mailable
 {
     use Queueable, SerializesModels;
 
-<<<<<<< HEAD
 
     public $cotizacion;
     public $usuario;
@@ -29,16 +28,6 @@ class CotizacionEtapa3Respuesta extends Mailable
         // $this->textMessage = 'Se ha enviado una cotización de servicio a su correo';
         $this->empresa = $empresa;
         $this->url = $url;
-=======
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
->>>>>>> 01fa2ca914a289a145b5ee0fc2b793feaecc94a0
     }
 
     /**
@@ -48,11 +37,7 @@ class CotizacionEtapa3Respuesta extends Mailable
      */
     public function build()
     {
-<<<<<<< HEAD
         return $this
         ->subject("Se ha Aceptado su Orden de Compra - ".config('app.name'))->markdown('emails.cotizacionEtapa3Respuesta');
-=======
-        return $this->view('view.name');
->>>>>>> 01fa2ca914a289a145b5ee0fc2b793feaecc94a0
     }
 }
